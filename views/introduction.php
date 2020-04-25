aaa35b52db0bec0cc219a69714d75760bd0b2c19
Chad <chadd_84@yahoo.com>
2020-04-24 21:45:34 -0700

    Added 3 basic cards with lorem ipsum

--- views/introduction.php
+++ views/introduction.php
@@ -36,51 +36,57 @@ SOFTWARE.
 <include href="views/parts/navbar.php"></include>
 
 <body>
-<div id="site-container">
-    <!--main carousel for the page-->
-    <div class="carousel slide" data-ride="carousel" id="carouselExampleIndicators">
-        <div class="carousel-inner text-center">
-            <!--first carousel-->
-            <div class="carousel-item active">
-                <img src="{{ @BASE }}/assets/img/coffee.jpg" class="img-fluid" alt="first image">
-                <div class="carousel-caption d-flex h-100  flex-column align-items-center justify-content-center">
-                    <h3>Looking for tech internships in Seattle-Tacoma?</h3>
-                    <br>
-                    <h5><a href="internships">Search Listings</a></h5>
-                </div>
-            </div>
-            <!--end first carousel-->
-            <!--second carousel-->
-            <div class="carousel-item">
-                <img src="{{ @BASE }}/assets/img/resource.jpg" class="img-fluid" alt="second image">
-                <div class="carousel-caption d-flex h-100  flex-column align-items-center justify-content-center">
-                    <h3>Need help with your resume and LinkedIn profile?</h3>
-                    <br>
-                    <h5 class="button"><a href="studentResources">Browse Resources</a></h5>
-                </div>
-            </div>
-            <!--end second carousel-->
-            <!--third carousel-->
-            <div class="carousel-item">
-                <img src="{{ @BASE }}/assets/img/session.jpg" class="img-fluid" alt="third image">
-                <div class="carousel-caption d-flex h-100  flex-column align-items-center justify-content-center">
-                    <h3>Seeking Career Development and Networking Opportunities?</h3>
-                    <br>
-                    <h5 class="button"><a href="https://www.meetup.com/South-King-Web-Mobile-Developers/events/" target="_blank">View events</a></h5>
-                </div>
-            </div>
-            <!--end third carousel-->
+<div id="navbar">
+
+    <!-- Internships -->
+    <div class="card">
+        <div class="card-header text-center">
+            Internships
         </div>
-        <!--indicator previous-->
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span aria-hidden="true"><i class="fas fa-arrow-circle-left fa-2x"></i></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <!--indicator next-->
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span aria-hidden="true"><i class="fas fa-arrow-circle-right fa-2x"></i></span>
-            <span class="sr-only">Next</span>
-        </a>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nisi ante, pellentesque sit amet mollis non,
+            placerat euismod odio. In hac habitasse platea dictumst. Suspendisse quis neque vitae eros imperdiet
+            consequat a ac tortor. Fusce augue sem, convallis et nulla malesuada, imperdiet molestie est. Pellentesque
+            vel varius ex, porttitor hendrerit justo. Vivamus nisl lectus, rutrum pulvinar venenatis id, fringilla ac
+            erat. Quisque venenatis risus turpis. Proin porta commodo libero vitae rhoncus. Mauris vestibulum magna ut
+            orci porta sollicitudin. Proin non est at sapien elementum cursus. Nulla ac maximus tellus. Vivamus feugiat
+            justo dolor, eget volutpat augue ullamcorper quis. Duis turpis massa, accumsan sit amet lacus et, commodo
+            ornare arcu. Vestibulum luctus pretium leo, eu iaculis ipsum pretium eget.
+        </div>
+    </div>
+
+    <!-- Meetups -->
+    <div class="card">
+        <div class="card-header text-center">
+            Meetups
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nisi ante, pellentesque sit amet mollis non,
+            placerat euismod odio. In hac habitasse platea dictumst. Suspendisse quis neque vitae eros imperdiet
+            consequat a ac tortor. Fusce augue sem, convallis et nulla malesuada, imperdiet molestie est. Pellentesque
+            vel varius ex, porttitor hendrerit justo. Vivamus nisl lectus, rutrum pulvinar venenatis id, fringilla ac
+            erat. Quisque venenatis risus turpis. Proin porta commodo libero vitae rhoncus. Mauris vestibulum magna ut
+            orci porta sollicitudin. Proin non est at sapien elementum cursus. Nulla ac maximus tellus. Vivamus feugiat
+            justo dolor, eget volutpat augue ullamcorper quis. Duis turpis massa, accumsan sit amet lacus et, commodo
+            ornare arcu. Vestibulum luctus pretium leo, eu iaculis ipsum pretium eget.
+        </div>
+    </div>
+
+    <!-- Blogs -->
+    <div class="card">
+        <div class="card-header text-center">
+            Blogs
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nisi ante, pellentesque sit amet mollis non,
+            placerat euismod odio. In hac habitasse platea dictumst. Suspendisse quis neque vitae eros imperdiet
+            consequat a ac tortor. Fusce augue sem, convallis et nulla malesuada, imperdiet molestie est. Pellentesque
+            vel varius ex, porttitor hendrerit justo. Vivamus nisl lectus, rutrum pulvinar venenatis id, fringilla ac
+            erat. Quisque venenatis risus turpis. Proin porta commodo libero vitae rhoncus. Mauris vestibulum magna ut
+            orci porta sollicitudin. Proin non est at sapien elementum cursus. Nulla ac maximus tellus. Vivamus feugiat
+            justo dolor, eget volutpat augue ullamcorper quis. Duis turpis massa, accumsan sit amet lacus et, commodo
+            ornare arcu. Vestibulum luctus pretium leo, eu iaculis ipsum pretium eget.
+        </div>
     </div>
 </div>
 </body>
