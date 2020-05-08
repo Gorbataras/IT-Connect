09aee287488c516271d91367215e766d9ea7fd00
Chad <chadd_84@yahoo.com>
2020-05-08 00:37:42 -0700

    Removed config file from website. Updated config path.

--- controllers/controller.php
+++ controllers/controller.php
@@ -35,7 +35,7 @@ function introduction($fatFree){
 	$response=file_get_contents('https://api.meetup.com/South-King-Web-Mobile-Developers/events?&sign=true&photo-host=public');
 	$response=json_decode($response);
 	$fatFree->set('array', $response);
-    $config = include("db/config.php");
+    $config = include("/home/nwagreen/config.php");
     $db = new PDO($config["db"], $config["username"], $config["password"]);
     $posts = new PostingsModel($db);
     $fatFree->set('posts', $posts->getAllPostings());
