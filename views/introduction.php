715db424630c63684b2e0be0413d4888a3fc28b1
Chad <chadd_84@yahoo.com>
2020-05-08 10:49:37 -0700

    Added more information to internships card

--- views/introduction.php
+++ views/introduction.php
@@ -81,8 +81,14 @@ SOFTWARE.
                                 <F3:repeat group="{{ @posts }}" value="{{ @post }}">
                                     <li class="list-group-item">
                                         <a href="{{@post->url}}" target="_blank" class="no-decoration"> <!-- Url column-->
-                                            <h4 class="company-name">{{@post->company}}</h4> <!-- Company column -->
-                                            <h6 class="category">{{@post->category}}</h6> <!-- Title column -->
+                                            <span class="company-name h4">{{@post->company}}</span> <!-- Company column -->
+                                            <br>
+                                            <span class="h6">{{@post->title}}</span>
+                                            <br>
+                                            <span>{{@post->location}}</span>
+                                            <br>
+                                            <span>Posted: {{@post->post_date}}</span>
+                                            <br>
                                             <p class="card-subtitle mb-2 text-muted"> <!-- Description column -->
                                                 <check if="{{empty(@post->description)}}">
                                                     <true>No description available</true>
@@ -120,13 +126,14 @@ SOFTWARE.
 							<ul id="latest-meetups" class="list-group list-group-flush">
 								<F3:repeat group="{{ @array }}" value="{{ @meetup }}">
 									<li class="list-group-item">
-										<a href="{{@meetup->link}}" target="_blank" class="no-decoration">
-												<h5 class="card-title">{{@meetup->name}}</h5>
-												<h6 class="card-subtitle mb-2 text-muted">
-													{{@meetup->local_time}} -> {{@meetup->local_date}}</h6>
-												<h6 class="card-subtitle mb-2 text-muted">
-													{{@meetup->venue->name}}</h6>
-										</a>
+                                        <a href="{{@meetup->link}}" target="_blank" class="no-decoration">
+                                            <span class="card-title h5">{{@meetup->name}}</span>
+                                            <br>
+                                            <span class="card-subtitle mb-2 text-muted h6">
+													{{@meetup->local_time}} -> {{@meetup->local_date}}</span>
+                                            <span class="card-subtitle mb-2 text-muted h6">
+													{{@meetup->venue->name}}</span>
+                                        </a>
 									</li>
 								</F3:repeat>
 							</ul>
